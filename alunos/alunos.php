ba1d6fef90b01a97919ad7f2a81b1ae8274f4e74
luiz eduardo <le6444329@gmail.com>
2026-09-21 15:39:44 -0300

    Add files via upload

---
+++ alunos/alunos.php
@@ -0,0 +1,52 @@
+<?php
+
+$msg = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$matricula = $_POST["matricula"];
+$cpf = $_POST["cpf"];
+$msg = "";
+
+echo "nome: " . $nome . " email: " . $email .  " matricula: " . $matricula . " cpf: " . $cpf;
+
+if (!file_exists("alunos.txt")) 
+{
+$arqAlun = fopen("alunos.txt","w") or die("erro ao criar arquivo");
+$linha = "nome;email;matricula;cpf\n";
+fwrite($arqAlun,$linha);
+fclose($arqAlun);
+}
+$arqAlun = fopen("alunos.txt","a") or die("erro ao criar arquivo");
+
+$linha = $nome . ";" . $email . ";" . $matricula . ";" . $cpf ."\n";
+fwrite($arqAlun,$linha);
+fclose($arqAlun);
+$msg = "Deu tudo certo!!!";
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+<body>
+<h1> Criar novo aluno</h1>
+<form method="POST">
+nome: <input type="text" name="nome">
+<br><br>
+email: <input type="text" name="email">
+<br><br>
+matricula: <input type="number" name="matricula">
+<br><br>
+cpf: <input type="number" name="cpf">
+<br><br>
+<input type="submit" value="Criar novo aluno">
+</form>
+<p><?php echo $msg ?></p>
+<br>
+</body>
+</html>
